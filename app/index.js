944ecf0d2e5f4cdaf4572d3135b95f9d94492fc3
OtaraAlex <mbechealex61@gmail.com>
2024-04-24 18:51:44 +0300

    Add the Job Details section and implement the search functionality

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { View, ScrollView, SafeAreaView } from "react-native";
 import { Stack, useRouter } from "expo-router";
 
@@ -10,7 +11,8 @@ import {
 } from "../components";
 
 const Home = () => {
-  const router = useRouter();
+  const router = useRouter()
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -31,7 +33,15 @@ const Home = () => {
 
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={{ flex: 1, padding: SIZES.medium }}>
-          <Welcome />
+          <Welcome 
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`)
+              }
+            }}
+          />
           <Popularjobs />
           <Nearbyjobs />
         </View>
