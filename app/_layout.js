788bfa25aef13f63997e1296c5baf78d0d729149
OtaraAlex <mbechealex61@gmail.com>
2024-04-24 14:35:15 +0300

    Project setup & home page

---
+++ app/_layout.js
@@ -0,0 +1,24 @@
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack initialRouteName="index">
+      <Stack.Screen name="index" />
+    </Stack>
+  )
+};
+
+export default RootLayout;
